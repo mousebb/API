1d8e0dbead2e9dbe7d813644b28c38c20d52f952
Alex Ninneman <alex@ninneman.org>
2015-12-22 17:16:57 -0600

    started on model tests for apiKeyType 94.1%

--- models/apiKeyType/apiKeyType.go
+++ models/apiKeyType/apiKeyType.go
@@ -3,48 +3,29 @@ package apiKeyType
 import (
 	"time"
 
+	"github.com/curt-labs/API/helpers/database"
 	"github.com/curt-labs/API/middleware"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 var (
-	getApiKeyType     = "SELECT id, type, date_added FROM ApiKeyType WHERE id = ? "
-	getAllApiKeyTypes = "SELECT id, type, date_added FROM ApiKeyType "
-	getKeyByDateType  = "SELECT id FROM ApiKeyType WHERE type = ?  AND date_added = ?"
-	createApiKeyType  = "INSERT INTO ApiKeyType (id, type, date_added) VALUES (UUID(),?,?)"
-	deleteApiKeyType  = "DELETE FROM ApiKeyType WHERE id = ? "
+	getAllKeyTypes = "SELECT id, type, date_added FROM ApiKeyType order by type"
 )
 
-type ApiKeyType struct {
+// KeyType Declares the type reference for an API key (Public, Private, Authentication).
+type KeyType struct {
 	ID        string    `json:"_id" xml:"id"`
 	Type      string    `json:"type" xml:"type"`
 	DateAdded time.Time `json:"dateAdded" xml:"dateAdded"`
-}
-
-type Scanner interface {
-	Scan(...interface{}) error
 }
 
 const (
 	timeFormat = "2006-01-02 03:04:05"
 )
 
-func (a *ApiKeyType) Get(ctx *middleware.APIContext) (err error) {
+// GetAllKeyTypes Returns a list of all the API key types in the database.
+func GetAllKeyTypes(ctx *middleware.APIContext) (as []KeyType, err error) {
 
-	stmt, err := ctx.DB.Prepare(getApiKeyType)
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-	res := stmt.QueryRow(a.ID)
-	a, err = ScanKey(res)
-
-	return
-}
-
-func GetAllApiKeyTypes(ctx *middleware.APIContext) (as []ApiKeyType, err error) {
-
-	stmt, err := ctx.DB.Prepare(getAllApiKeyTypes)
+	stmt, err := ctx.DB.Prepare(getAllKeyTypes)
 	if err != nil {
 		return
 	}
@@ -55,7 +36,7 @@ func GetAllApiKeyTypes(ctx *middleware.APIContext) (as []ApiKeyType, err error) 
 	}
 
 	for res.Next() {
-		a, err := ScanKey(res)
+		a, err := scan(res)
 		if err != nil {
 			return as, err
 		}
@@ -65,49 +46,8 @@ func GetAllApiKeyTypes(ctx *middleware.APIContext) (as []ApiKeyType, err error) 
 	return as, err
 }
 
-func ScanKey(s Scanner) (*ApiKeyType, error) {
-	a := &ApiKeyType{}
+func scan(s database.Scanner) (*KeyType, error) {
+	a := &KeyType{}
 	err := s.Scan(&a.ID, &a.Type, &a.DateAdded)
 	return a, err
-}
-
-func (a *ApiKeyType) Create(ctx *middleware.APIContext) (err error) {
-
-	stmt, err := ctx.DB.Prepare(createApiKeyType)
-	if err != nil {
-		return
-	}
-	defer stmt.Close()
-	added := time.Now().Format(timeFormat)
-	_, err = stmt.Exec(a.Type, added)
-	if err != nil {
-		return
-	}
-
-	stmt, err = ctx.DB.Prepare(getKeyByDateType)
-	if err != nil {
-		return err
-	}
-
-	defer stmt.Close()
-
-	err = stmt.QueryRow(a.Type, added).Scan(&a.ID)
-	if err != nil {
-		return err
-	}
-	return
-}
-
-func (a *ApiKeyType) Delete(ctx *middleware.APIContext) (err error) {
-
-	stmt, err := ctx.DB.Prepare(deleteApiKeyType)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(a.ID)
-	if err != nil {
-		return err
-	}
-	return
 }
--- models/apiKeyType/apiKeyType_test.go
+++ models/apiKeyType/apiKeyType_test.go
@@ -1,69 +1,141 @@
 package apiKeyType
 
 import (
-	. "github.com/smartystreets/goconvey/convey"
+	"database/sql"
+	"log"
 	"testing"
+	"time"
+
+	"github.com/curt-labs/API/middleware"
+	"github.com/julienschmidt/httprouter"
+	"github.com/ory-am/dockertest"
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func TestApiKeyType(t *testing.T) {
-	Convey("Test Create AppGuide", t, func() {
+var (
+	db *sql.DB
+
+	schemas = map[string]string{
+		`apiKeyTypeSchema`: `CREATE TABLE ApiKeyType (id varchar(64) NOT NULL,type varchar(500) DEFAULT NULL,date_added timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,PRIMARY KEY (id)) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT`,
+	}
+
+	dataInserts = map[string]string{
+		`insertKeyType`: `INSERT INTO ApiKeyType (id, type, date_added) VALUES (UUID(),'random type', NOW())`,
+	}
+)
+
+func TestMain(m *testing.M) {
+
+	mysql, err := dockertest.ConnectToMySQL(15, time.Second*5, func(url string) bool {
 		var err error
-		var akt ApiKeyType
+		db, err = sql.Open("mysql", url+"?parseTime=true")
+		if err != nil {
+			log.Fatalf("MySQL connection failed, with address '%s'.", url)
+		}
 
-		//create
-		akt.Type = "testType"
+		for _, schema := range schemas {
+			_, err = db.Exec(schema)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 
-		err = akt.Create()
-		So(err, ShouldBeNil)
+		return db.Ping() == nil
+	})
 
-		//get
-		err = akt.Get()
-		So(err, ShouldBeNil)
+	defer func() {
+		db.Close()
+		mysql.KillRemove()
+	}()
 
-		as, err := GetAllApiKeyTypes()
-		So(err, ShouldBeNil)
-		So(len(as), ShouldBeGreaterThan, 0)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-		//delete
-		err = akt.Delete()
-		So(err, ShouldBeNil)
+	m.Run()
+}
+
+func insertKeys() error {
+	for _, insert := range dataInserts {
+		_, err := db.Exec(insert)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func TestGetAllKeyTypes(t *testing.T) {
+	Convey("Test GetAllKeyTypes", t, func() {
+		ctx := &middleware.APIContext{
+			DataContext: &middleware.DataContext{
+				BrandID: 3,
+			},
+			Params: httprouter.Params{},
+			DB:     db,
+		}
+
+		Convey("with invalid DB query", func() {
+			tmp := getAllKeyTypes
+			getAllKeyTypes = "invalid database query"
+
+			as, err := GetAllKeyTypes(ctx)
+			So(err, ShouldNotBeNil)
+			So(as, ShouldBeNil)
+
+			getAllKeyTypes = tmp
+		})
+
+		// Convey("with no data", func() {
+		// 	tmp := getAllKeyTypes
+		// 	getAllKeyTypes = "truncate table ApiKeyType"
+		//
+		// 	as, err := GetAllKeyTypes(ctx)
+		// 	So(err, ShouldBeNil)
+		// 	So(as, ShouldBeNil)
+		//
+		// 	getAllKeyTypes = tmp
+		// })
+
+		Convey("with missing select columns", func() {
+			tmp := getAllKeyTypes
+			getAllKeyTypes = "SELECT id, type FROM ApiKeyType order by type"
+
+			err := insertKeys()
+			So(err, ShouldBeNil)
+
+			as, err := GetAllKeyTypes(ctx)
+			So(err, ShouldNotBeNil)
+			So(as, ShouldBeNil)
+
+			getAllKeyTypes = tmp
+		})
+
+		Convey("with valid DB connection", func() {
+
+			err := insertKeys()
+			So(err, ShouldBeNil)
+
+			as, err := GetAllKeyTypes(ctx)
+			So(err, ShouldBeNil)
+			So(len(as), ShouldBeGreaterThan, 0)
+
+		})
+
 	})
 }
 
-func BenchmarkGetAllApiKeyTypes(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		GetAllApiKeyTypes()
+func BenchmarkGetAllKeyTypes(b *testing.B) {
+	ctx := &middleware.APIContext{
+		DataContext: &middleware.DataContext{
+			BrandID: 3,
+		},
+		Params: httprouter.Params{},
+		DB:     db,
 	}
-}
 
-func BenchmarkGetApiKeyType(b *testing.B) {
-	akt := ApiKeyType{Type: "TESTER"}
 	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		akt.Create()
-		b.StartTimer()
-		akt.Get()
-		b.StopTimer()
-		akt.Delete()
-	}
-}
-
-func BenchmarkCreateApiKeyType(b *testing.B) {
-	akt := ApiKeyType{Type: "TESTER"}
-	for i := 0; i < b.N; i++ {
-		b.StartTimer()
-		akt.Create()
-		b.StopTimer()
-		akt.Delete()
-	}
-}
-
-func BenchmarkDeleteApiKeyType(b *testing.B) {
-	akt := ApiKeyType{Type: "TESTER"}
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		akt.Create()
-		b.StartTimer()
-		akt.Delete()
+		GetAllKeyTypes(ctx)
 	}
 }
