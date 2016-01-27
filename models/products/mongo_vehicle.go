ad38494f7aa82e7b5f0afc48955049af1de40f23
Alex Ninneman <alex@ninneman.org>
2016-01-26 22:19:17 -0600

    i think it works? curious about network performance, thats a beast of a call

--- models/products/mongo_vehicle.go
+++ models/products/mongo_vehicle.go
@@ -2,7 +2,6 @@ package products
 
 import (
 	"fmt"
-	"log"
 	"sort"
 	"strings"
 
@@ -47,7 +46,7 @@ func (va *VehicleApplication) Query(ctx *middleware.APIContext) error {
 	} else if va.Year != "" && va.Make != "" && va.Model == "" {
 		va.Models, err = getModels(ctx, va.Year, va.Make)
 	} else if va.Year != "" && va.Make != "" && va.Model != "" {
-		_, err = getStyles(ctx, va.Year, va.Make, va.Model)
+		va.CategoryStyles, err = getStyles(ctx, va.Year, va.Make, va.Model)
 	}
 
 	return err
@@ -207,7 +206,7 @@ func getModels(ctx *middleware.APIContext, year, vehicleMake string) ([]string, 
 	return models, err
 }
 
-func getStyles(ctx *middleware.APIContext, year, vehicleMake, model string) ([]string, error) {
+func getStyles(ctx *middleware.APIContext, year, vehicleMake, model string) ([]CategoryStyle, error) {
 	if ctx.Session == nil {
 		return nil, fmt.Errorf("invalid mongodb connection")
 	} else if ctx.DataContext == nil {
@@ -221,7 +220,7 @@ func getStyles(ctx *middleware.APIContext, year, vehicleMake, model string) ([]s
 		PartNum string               `bson:"part_number"`
 	}
 
-	var apps []Apps
+	var parts []Part
 	err := c.Find(bson.M{
 		"vehicle_applications": bson.M{
 			"$elemMatch": bson.M{
@@ -242,27 +241,77 @@ func getStyles(ctx *middleware.APIContext, year, vehicleMake, model string) ([]s
 		"brand.id": bson.M{
 			"$in": ctx.DataContext.BrandArray,
 		},
-	}).All(&apps)
+	}).All(&parts)
 
 	if err != nil {
 		return nil, err
 	}
 
-	var styles []string
+	lowerMake := strings.ToLower(vehicleMake)
+	lowerModel := strings.ToLower(model)
 
-	existing := make(map[string]string, 0)
-	for _, app := range apps {
-		log.Println(app.PartNum)
-		for _, a := range app.Apps {
-			a.Style = strings.Title(a.Style)
-			if _, ok := existing[a.Style]; !ok {
-				styles = append(styles, a.Style)
-				existing[a.Style] = a.Style
+	css := make(map[string]CategoryStyle, 0)
+	for _, p := range parts {
+		if len(p.Categories) == 0 {
+			continue
+		}
+
+		for _, va := range p.Vehicles {
+			if va.Year != year || strings.ToLower(va.Make) != lowerMake || strings.ToLower(va.Model) != lowerModel {
+				continue
 			}
+
+			css = mapPartToCategoryStyles(p, css, va.Style)
+		}
+
+	}
+
+	var catStyles []CategoryStyle
+	for _, cs := range css {
+		catStyles = append(catStyles, cs)
+	}
+
+	return catStyles, nil
+}
+
+func getChildCategory(cats []Category) (Category, error) {
+	for _, cat := range cats {
+		if len(cat.Children) == 0 {
+			return cat, nil
 		}
 	}
 
-	sort.Strings(styles)
+	return Category{}, fmt.Errorf("failed to locate child")
+}
 
-	return styles, err
+func mapPartToCategoryStyles(p Part, css map[string]CategoryStyle, style string) map[string]CategoryStyle {
+	childCat, err := getChildCategory(p.Categories)
+	if err != nil || childCat.Identifier.String() == "" {
+		return css
+	}
+
+	cs, ok := css[childCat.Identifier.String()]
+	if !ok {
+		cs = CategoryStyle{
+			Category: childCat,
+		}
+	}
+
+	for i, sp := range cs.StyleParts {
+		if strings.ToLower(sp.Style) == strings.ToLower(style) {
+			cs.StyleParts[i].Parts = append(cs.StyleParts[i].Parts, p)
+			css[childCat.Identifier.String()] = cs
+			return css
+		}
+	}
+
+	currentStyle := StyleParts{
+		Style: strings.Title(style),
+		Parts: []Part{p},
+	}
+
+	cs.StyleParts = append(cs.StyleParts, currentStyle)
+	css[childCat.Identifier.String()] = cs
+
+	return css
 }
